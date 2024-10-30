746f5a57734d141e4b75ad8aa564665a38cc7a9f
lucassGiordano <164955947+lucassGiordano@users.noreply.github.com>
2024-10-30 12:51:38 -0300

    Update GameState.java

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -12,7 +12,6 @@ public class GameState {
 	private Player player;
 	
 	private Enemigo enemigo;
-	
 	public GameState() 
 	{
 		player = new Player(new Vector2D(100,100),Assets.player);
@@ -23,6 +22,7 @@ public class GameState {
 	{
 		player.update();
 		enemigo.update();
+		colition(player.getPosition(),new Vector2D(100,200));
 	}
 	//caracteristicas en comun de objetos 
 	//como puede ser posicion
@@ -30,6 +30,14 @@ public class GameState {
 	{
 		player.draw(g);
 		enemigo.draw(g);
+	}
+	public void colition(Vector2D a, Vector2D b) {
+		if(a.getX() < b.getX()+50 && a.getX() > b.getX() ) {
+			System.out.println("colision");	
+			
+		}
+		System.out.println(a.getX());	
+		
 	}
 
 }
